8320758f3ff739f656c3420b9983247f88e8a8a1
Justin Ridgewell <justin@ridgewell.name>
2022-01-22 06:30:35 -0500

    Comments and cleanup

--- src/resolve-uri.ts
+++ src/resolve-uri.ts
@@ -1,18 +1,15 @@
-// Matches the scheme of a URL, which is a strong indicator it is an valid
-// absolute URL.
-const schemeRegex = /^[\w+.-]+:\/\//i;
-const urlRegex = /^([\w+.-]+:)\/\/([^@]*@)?([^:/]+)(:\d+)?(\/[^#?]*)?/;
+// Matches the scheme of a URL, eg "http://"
+const schemeRegex = /^[\w+.-]+:\/\//;
 
 /**
- * Removes the filename from the path (everything trailing the last "/"). This
- * is only safe to call on a path, never call with an absolute or protocol
- * relative URL.
+ * Matches the parts of a URL:
+ * 1. Scheme, including ":", guaranteed.
+ * 2. User/password, including "@", optional.
+ * 3. Host, guaranteed.
+ * 4. Port, including ":", optional.
+ * 5. Path, including "/", optional.
  */
-function stripPathFilename(path: string): string {
-  if (path.endsWith('/..')) return path;
-  const index = path.lastIndexOf('/');
-  return path.slice(0, index + 1);
-}
+const urlRegex = /^([\w+.-]+:)\/\/([^@]*@)?([^:/]+)(:\d+)?(\/[^#?]*)?/;
 
 type Url = {
   scheme: string;
@@ -35,25 +32,7 @@ function isAbsolutePath(input: string): boolean {
   return input.startsWith('/');
 }
 
-function parseUrl(input: string): Url {
-  if (isSchemeRelativeUrl(input)) {
-    const url = parseUrl('http:' + input);
-    url.scheme = '';
-    return url;
-  }
-  if (isAbsolutePath(input)) {
-    const url = parseUrl('http://foo.com' + input);
-    url.scheme = '';
-    url.host = '';
-    return url;
-  }
-  if (!isAbsoluteUrl(input)) {
-    const url = parseUrl('http://foo.com/' + input);
-    url.scheme = '';
-    url.host = '';
-    url.relativePath = true;
-    return url;
-  }
+function parseAbsoluteUrl(input: string): Url {
   const match = urlRegex.exec(input)!;
   const host = match[3];
   return {
@@ -66,49 +45,100 @@ function parseUrl(input: string): Url {
   };
 }
 
-function mergePaths(url: Url, base: Url) {
-  if (url.path) {
-    if (!url.relativePath) return;
-    normalizePath(base);
-    url.path = stripPathFilename(base.path) + url.path;
-  } else {
-    url.path = base.path;
+function parseUrl(input: string): Url {
+  if (isSchemeRelativeUrl(input)) {
+    const url = parseAbsoluteUrl('http:' + input);
+    url.scheme = '';
+    return url;
   }
+  if (isAbsolutePath(input)) {
+    const url = parseAbsoluteUrl('http://foo.com' + input);
+    url.scheme = '';
+    url.host = '';
+    return url;
+  }
+  if (!isAbsoluteUrl(input)) {
+    const url = parseAbsoluteUrl('http://foo.com/' + input);
+    url.scheme = '';
+    url.host = '';
+    url.relativePath = true;
+    return url;
+  }
+  return parseAbsoluteUrl(input);
+}
+
+function stripPathFilename(path: string): string {
+  // If a path ends with a parent directory "..", then it's a relative path with excess parent
+  // paths. It's not a file, so we can't strip it.
+  if (path.endsWith('/..')) return path;
+  const index = path.lastIndexOf('/');
+  return path.slice(0, index + 1);
+}
+
+function mergePaths(url: Url, base: Url) {
+  // If we're not a relative path, then we're an absolute path, and it doesn't matter what base is.
+  if (!url.relativePath) return;
+
+  normalizePath(base);
+  // Resolution happens relative to the base path's directory, not the file.
+  url.path = stripPathFilename(base.path) + url.path;
+  // If the base path is absolute, then our path is now absolute too.
   url.relativePath = base.relativePath;
 }
 
-function copyDomainIfEmpty(url: Url, base: Url) {
-  if (url.host) return;
-  url.user = base.user;
-  url.host = base.host;
-  url.port = base.port;
-}
-
+/**
+ * The path can have empty directories "//", unneeded parents "foo/..", or current directory
+ * "foo/.". We need to normalize to a standard representation.
+ */
 function normalizePath(url: Url) {
   const { relativePath } = url;
   const pieces = url.path.split('/');
 
+  // We need to preserve the first piece always, so that we output a leading slash. The item at
+  // pieces[0] is an empty string.
   let pointer = 1;
+
+  // Positive is the number of real directories we've output, used for popping a parent directory.
+  // Eg, "foo/bar/.." will have a positive 2, and we can decrement to be left with just "foo".
   let positive = 0;
+
+  // We need to keep a trailing slash if we encounter an empty directory (eg, splitting "foo/" will
+  // generate `["foo", ""]` pieces). And, if we pop a parent directory. But once we encounter a
+  // real directory, we won't need to append, unless the other conditions happen again.
   let addTrailingSlash = false;
+
   for (let i = 1; i < pieces.length; i++) {
     const piece = pieces[i];
+
+    // An empty directory, could be a trailing slash, or just a double "//" in the path.
     if (!piece) {
       addTrailingSlash = true;
       continue;
     }
+
+    // If we encounter a real directory, then we don't need to append anymore.
     addTrailingSlash = false;
+
+    // A current directory, which we can always drop.
     if (piece === '.') continue;
+
+    // A parent directory, we need to see if there are any real directories we can pop. Else, we
+    // have an excess of parents, and we'll need to keep the "..".
     if (piece === '..') {
       if (positive) {
         addTrailingSlash = true;
         positive--;
         pointer--;
       } else if (relativePath) {
+        // If we're in a relativePath, then we need to keep the excess parents. Else, in an absolute
+        // URL, protocol relative URL, or an absolute path, we don't need to keep excess.
         pieces[pointer++] = piece;
       }
       continue;
     }
+
+    // We've encountered a real directory. Move it to the next insertion pointer, which accounts for
+    // any popped or dropped directories.
     pieces[pointer++] = piece;
     positive++;
   }
@@ -121,29 +151,40 @@ function normalizePath(url: Url) {
   url.path = path;
 }
 
-function print(url: Url, keepRelative: boolean): string {
-  if (url.relativePath) {
-    const path = url.path.slice(1);
-    return !keepRelative || path.startsWith('.') ? path : './' + path;
-  }
-  if (!url.host) return url.path;
-  return `${url.scheme}//${url.user}${url.host}${url.port}${url.path}`;
-}
-
 /**
- * Attempts to resolve `input` URL relative to `base`.
+ * Attempts to resolve `input` URL/path relative to `base`.
  */
 export default function resolve(input: string, base: string | undefined): string {
-  const keepRelative = (base || input).startsWith('.');
   const url = parseUrl(input);
 
+  // If we have a base, and the input isn't already an absolute URL, then we need to merge.
   if (base && !url.scheme) {
     const baseUrl = parseUrl(base);
     url.scheme = baseUrl.scheme;
-    copyDomainIfEmpty(url, baseUrl);
+    // If there's no host, then we were just a path.
+    if (!url.host) {
+      // The host, user, and port are joined, you can't copy one without the others.
+      url.user = baseUrl.user;
+      url.host = baseUrl.host;
+      url.port = baseUrl.port;
+    }
     mergePaths(url, baseUrl);
   }
 
   normalizePath(url);
-  return print(url, keepRelative);
+
+  // If the input (and base, if there was one) are both relative, then we need to output a relative.
+  if (url.relativePath) {
+    // The first char is always a "/".
+    const path = url.path.slice(1);
+    // If base started with a leading ".", or there is no base and input started with a ".", then we
+    // need to ensure that the relative path starts with a ".". We don't know if relative starts
+    // with a "..", though, so check before prepending.
+    const keepRelative = (base || input).startsWith('.');
+    return !keepRelative || path.startsWith('.') ? path : './' + path;
+  }
+  // If there's no host (and no scheme/user/port), then we need to output an absolute path.
+  if (!url.host) return url.path;
+  // We're outputting either an absolute URL, or a protocol relative one.
+  return `${url.scheme}//${url.user}${url.host}${url.port}${url.path}`;
 }
